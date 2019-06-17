69d84178b30874062d810560c2aec666c196c38d
Xiaoyu Sun <xisun@localhost.localdomain>
2019-06-17 15:12:05 +0200

    Added some useful comments

--- MultiLevelEvacuation/code/initEscapeRoutes.m
+++ MultiLevelEvacuation/code/initEscapeRoutes.m
@@ -11,13 +11,14 @@ for i=1:data.floor_count
     %else
         boundary_data(data.floor(i).img_stairs_down) = -1;
     %end
-    
-    boundary_data_coop = boundary_data;
-    boundary_data_coop(data.floor(i).img_imaginary) = 1;
 
     exit_dist = fastSweeping(boundary_data) * data.meter_per_pixel;
     [data.floor(i).img_dir_x, data.floor(i).img_dir_y] = ...
         getNormalizedGradient(boundary_data, -exit_dist);
+    
+    % imaginary walls affect only cooperators
+    boundary_data_coop = boundary_data;
+    boundary_data_coop(data.floor(i).img_imaginary) = 1;
     
     exit_dist_coop = fastSweeping(boundary_data_coop) * data.meter_per_pixel;
     [data.floor(i).img_dir_x_coop, data.floor(i).img_dir_y_coop] = ...
--- MultiLevelEvacuation/code/lerp2.c
+++ MultiLevelEvacuation/code/lerp2.c
@@ -7,6 +7,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     double *data, *out, x, y, wx0, wy0, wx1, wy1;
     double d00, d01, d10, d11;
     
+    // Check number of in-/outputs
     if (nlhs < 1)
         return;
     else if (nlhs > 1)
@@ -18,20 +19,25 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
     m = mxGetM(prhs[0]); // number of rows of 2-D array
     n = mxGetN(prhs[0]); // number of columns of 2-D array
     data = mxGetPr(prhs[0]); // pointer to data
-    x = *mxGetPr(prhs[1]) - 1;
+    x = *mxGetPr(prhs[1]) - 1; // C counts from 0, matlab counts from 1
     y = *mxGetPr(prhs[2]) - 1;
     
     plhs[0] = mxCreateDoubleMatrix(1, 1, mxREAL); // make output matrix
     out = mxGetPr(plhs[0]); // pointer to output matrix
     
+    // if x or y out of boundary, put it back inside
     x = x < 0 ? 0 : x > m - 1 ? m - 1 : x;
     y = y < 0 ? 0 : y > n - 1 ? n - 1 : y;
+    
     i0 = (int) x;
     j0 = (int) y;
+    
+    /* This part seems unnecessary. Simulations runs perfectly without it
     i1 = i0 + 1;
     i1 = i1 > m - 1 ? m - 1 : i1;
     j1 = j0 + 1;
     j1 = j1 > n - 1 ? n - 1 : j1;
+    */
     
     idx00 = i0 + m * j0;
     d00 = data[idx00];
