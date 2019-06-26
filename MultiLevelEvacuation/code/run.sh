d1f03940255268287d0c29f5afb06fc9f28a3586
Jingqiu Ding <jding@eu-login-15-ng.euler.ethz.ch>
2019-06-26 18:06:16 +0200

    script modify

--- MultiLevelEvacuation/code/run.sh
+++ MultiLevelEvacuation/code/run.sh
@@ -1,5 +1,11 @@
 for i in {0..6}
 do
-bsub -n 12 -W 1:00 -R "rusage[mem=512]" matlab -nodisplay -singleCompThread -r "A_LearningAlgorithm($i,16,12)"
+for iteration in 16
+do 
+for children in 12
+do
+bsub -n 12 -W 1:00 -R "rusage[mem=512]" matlab -nodisplay -singleCompThread -r "A_LearningAlgorithm($i,$iteration,$children)"
+done
+done
 done
 
